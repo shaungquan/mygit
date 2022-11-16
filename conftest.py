7df513bc1d87f8f7d1dca68426bdfcdf6f367ea2
89398664@qq.com <89398664@qq.com>
2022-11-16 15:07:46 +0800

    优化了密码加密功能

--- conftest.py
+++ conftest.py
@@ -17,9 +17,15 @@ def clear_yaml():
 
 
 # 密码加密
+data = YamlUtil().read_tesecase_yaml("login.yml")
 @pytest.fixture(scope="function")
 def pw_encryption(request):
-    h1 = hashlib.md5()
-    h1.update(request.param.encode('utf-8'))
-    d1 = h1.digest()  # 返回二进制数据字符串值
-    return base64.b64encode(d1).decode('utf-8')
+    pw = request.param['request']['data']['password']
+    pw1 = ""
+    if pw != None:
+        h1 = hashlib.md5()
+        h1.update(pw.encode('utf-8'))
+        d1 = h1.digest()  # 返回二进制数据字符串值
+        pw1 = base64.b64encode(d1).decode('utf-8')
+    return pw1
+    # return request.param
--- testcases/test_send_request.py
+++ testcases/test_send_request.py
@@ -1,45 +1,60 @@
 import requests
 import pytest
 import json
+
+import encryption
 from common.yaml_util import YamlUtil
+import hashlib
+import base64
+
+
+# 加密密码
+def pw_encryption(pw=None):
+    h1 = hashlib.md5()
+    h1.update(pw.encode('utf-8'))
+    d1 = h1.digest()  # 返回二进制数据字符串值
+    return base64.b64encode(d1).decode('utf-8')
 
 
 class TestSendRequest:
     session = requests.session()
     app_key = "da6a9d132a861f28bd6e8c1b8b31f422"
     identify_key = "3a58782c204aef4cb984f8e4a1e682f9"
-    login_data = YamlUtil().read_tesecase_yaml('login.yml')
-    print("这是数据", login_data)
-    pw = login_data[0]["request"]["data"]["password"].split()
-    @pytest.mark.parametrize("pw_encryption", login_data[0]["request"]["data"]["password"].split(), indirect=True)
-    def test_login(self, pw_encryption):
-
-        print("这是加密后的密码：", pw_encryption)
-#         pw = pw_encryption
-#         user_name = "18281740124"
-#         param = {
-#             "app_key": TestSendRequest.app_key,
-#             "identify_key": TestSendRequest.identify_key,
-#             "is_new": 0,
-#             "login_type": "2",
-#             "password": pw,
-#             "platform": "1",
-#             "user_name": user_name
-# }
-#         headers = {
-#             'X-Requested-With': 'XMLHttpRequest',
-#             'Content-Type': 'application/x-www-form-urlencoded'
-#         }
-#         # 字典转字符串，禁止中文转Unicode格式11
-#         va = json.dumps(param, ensure_ascii=False)
-#         # # 去掉字符串中的空格
-#         va1 = ''.join(va.split())
-#         url = "http://apis.183read.cc/open_api5/rest6.php?act=member.user.enter&param={}".format(va1)
-#         re = TestSendRequest.session.request("post", url=url, headers=headers)
-#         YamlUtil().write_excract_yaml({"token": re.json()["result"]["member_info"]["token"]})
-#         print(re.json())
-
-    def test1_base(self):
+    @pytest.mark.parametrize("caseinfo", YamlUtil().read_tesecase_yaml('login.yml'))
+    # @pytest.mark.parametrize("pw_encryption", YamlUtil().read_tesecase_yaml('login.yml')[0], indirect=True)
+    def test_login(self, caseinfo):
+        pw = encryption.common().pw_encryption(caseinfo['request']['data']['password'])
+        print("这是", pw)
+        pw = pw_encryption(caseinfo['request']['data']['password'])
+        user_name = caseinfo['request']['data']['user_name']
+        method = caseinfo['request']['method']
+        ces = caseinfo['validata']
+        print(ces)
+        param = {
+            "app_key": TestSendRequest.app_key,
+            "identify_key": TestSendRequest.identify_key,
+            "is_new": 0,
+            "login_type": "2",
+            "password": pw,
+            "platform": "1",
+            "user_name": user_name
+}
+        headers = {
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        # 字典转字符串，禁止中文转Unicode格式11
+        va = json.dumps(param, ensure_ascii=False)
+        # # 去掉字符串中的空格
+        va1 = ''.join(va.split())
+        url = "http://apis.183read.cc/open_api5/rest6.php?act=member.user.enter&param={}".format(va1)
+        re = TestSendRequest.session.request(method, url=url, headers=headers)
+        print("这是返回的数据：", re.json())
+        if "member_info" in re.json()["result"]:
+            YamlUtil().write_excract_yaml({"token": re.json()["result"]["member_info"]["token"]})
+        else:
+            assert caseinfo['validata'] == re.json()['result']['status_info']['status_message']
+    def test_base(self):
         token = YamlUtil().read_extract_yaml("token")
         param = {
             "app_key": TestSendRequest.app_key,
